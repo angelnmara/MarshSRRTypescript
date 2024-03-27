c292e263e2e11781b549f9f0ed035b7ce088c4d4
David Rincón <david.rincon@prototipo0.com>
2024-03-27 12:03:06 -0600

    se agrega conexion con mongo db en usuarios

--- src/infraestructure/dependencies.ts
+++ src/infraestructure/dependencies.ts
@@ -1,7 +1,7 @@
 import { UsuariosService } from "../application/service/UsuariosService";
 import { UsuariosController } from "./controller/UsuariosController";
-import { FileUsuariosRepository } from "./mongo-repository/FileUsuariosRepository";
+import { MongoUsuariosRepository } from "./mongo-repository/MongoUsuariosRepository";
 
-const mongoUsuariosRepository = new FileUsuariosRepository();
+const mongoUsuariosRepository = new MongoUsuariosRepository;
 const usuariosService = new UsuariosService(mongoUsuariosRepository);
 export const usuariosController = new UsuariosController(usuariosService);
--- src/infraestructure/mongo-repository/MongoUsuariosRepository.ts
+++ src/infraestructure/mongo-repository/MongoUsuariosRepository.ts
@@ -1,12 +1,25 @@
+import { MongoClient } from "mongodb";
 import { Usuarios } from "../../domain/entity/Usuarios";
 import { UsuariosRepository } from "../../domain/repository/UsuariosRepository";
 
+const uri = "mongodb+srv://angelnmara:sD7mLAZ0Bi0P0LPK@cluster0.0913r1x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
+const database = client.db("insertDB");
+    // Specifying a Schema is optional, but it enables type hints on
+    // finds and inserts
+    const usuariosMDB = database.collection<Usuarios>("usuarios");
+
 export class MongoUsuariosRepository implements UsuariosRepository {
   async getById(id: string): Promise<Usuarios | null> {
-    throw new Error("Method not implemented.");
+    const usuarioMDB = await usuariosMDB.findOne<Usuarios>({ IdUsuario: id }, {
+        sort: { IdUsuario: -1 }
+      });
+    return usuarioMDB;
   }
-  save(usuarios: Usuarios): Promise<Usuarios> {
-    throw new Error("Method not implemented.");
+  async save(usuarios: Usuarios): Promise<Usuarios> {
+    await usuariosMDB.insertOne(usuarios);
+    return usuarios;
   }
   getAll(): Promise<Usuarios[]> {
     throw new Error("Method not implemented.");
